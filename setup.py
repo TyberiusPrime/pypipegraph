6a95ebf6e02658d3a23367da5441fccf6364cff6
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2012-01-30 15:30:28 +0100

    tuning metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='pypipegraph',
-    version='0.126',
+    version='0.128',
     packages=['pypipegraph',],
     license='MIT',
     url='http://code.google.com/p/pypipegraph/',
     author='Florian Finkernagel',
     author_email='finkernagel@imt.uni-marburg.de',
-    summary = "A workflow (job) engine/pipeline for bioinformatics.",
+    description = "A workflow (job) engine/pipeline for bioinformatics.",
     long_description=open('README.txt').read(),
 )
