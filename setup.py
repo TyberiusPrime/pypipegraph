52a2365cd1c64aa9232c8f29111894ccb138c06d
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2012-01-30 15:26:01 +0100

    email-adress typo

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='pypipegraph',
-    version='0.1',
+    version='0.126',
     packages=['pypipegraph',],
-    license='BSD',
+    license='MIT',
     url='http://code.google.com/p/pypipegraph/',
     author='Florian Finkernagel',
-    author_email='finkernagel@coonabibba.de',
+    author_email='finkernagel@imt.uni-marburg.de',
+    summary = "A workflow (job) engine/pipeline for bioinformatics.",
     long_description=open('README.txt').read(),
 )
