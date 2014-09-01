8aeecd6e0db6f27fc2a7e244cd387218fef89f59
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2014-09-01 15:39:12 +0200

    test.py now correctly calculates coverage of jobs

--- pypipegraph/tests/test.py
+++ pypipegraph/tests/test.py
@@ -1,21 +1,36 @@
-from multiprocessing import util
-import coverage
-import unittest
+modules_to_test = ['test_pypipegraph']
+
 import os
+os.environ['PYPIPEGRAPH_DO_COVERAGE'] = 'True'
+import coverage
+with open(".coveragerc",'wb') as op:
+    op.write("""
+[run]
+data_file = /code/pypipegraph/pypipegraph/tests/.coverageX
+parallel=True
+[report]
+include = *pypipegraph*
+    """) 
+
+
+cov = coverage.coverage(source = [os.path.abspath('../')], config_file = '.coveragerc', data_suffix=True)
+cov.start()
+
+import unittest
 import nose
 import noseprogressive
 import sys
+sys.path.append('/code/pypipegraph/')
+import pypipegraph as ppg
 
-coverage.process_startup()
-cov = coverage.coverage(source = [os.path.abspath('../')])
-cov.start()
-import test_pypipegraph
 try:
-    nose.core.runmodule('test_pypipegraph', argv=sys.argv + ['--with-progressive'], exit=False)
+    nose.core.runmodule(modules_to_test, argv=sys.argv + ['--with-progressive'], exit=False)
 except Exception, e:
+    print' error'
     print e
     pass
-print 'now doing coverage report'
 cov.stop()
-cov.html_report(directory='covhtml')
-print 'done, result in covhtml/index.html'
+cov.save()
+os.system('coverage combine')
+os.system('coverage html -d covhtml')
+print 'coverage report is in covhtml/index.html'
