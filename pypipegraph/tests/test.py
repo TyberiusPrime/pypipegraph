7144485859eafba495e01e9c2045f57f26900ad9
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2014-09-04 12:00:39 +0200

    beginning python 3 support, no longer dumping graph in tests that don't test that

--- pypipegraph/tests/test.py
+++ pypipegraph/tests/test.py
@@ -4,7 +4,7 @@ import os
 os.environ['PYPIPEGRAPH_DO_COVERAGE'] = '/code/pypipegraph/pypipegraph/tests/.coveragerc'
 import coverage
 with open(".coveragerc",'wb') as op:
-    op.write("""
+    op.write(b"""
 [run]
 data_file = /code/pypipegraph/pypipegraph/tests/.coverageX
 parallel=True
@@ -27,12 +27,12 @@ try:
     nose.core.runmodule(modules_to_test, argv=sys.argv + ['--with-progressive', 
         '--nologcapture'
         ], exit=False)  #log capture get's a ton of output from the pipegraph... enable if you need it
-except Exception, e:
-    print' error'
-    print e
+except Exception as e:
+    print ('error')
+    print (e)
     pass
 cov.stop()
 cov.save()
 os.system('coverage combine')
 os.system('coverage html -d covhtml')
-print 'coverage report is in covhtml/index.html'
+print ('coverage report is in covhtml/index.html')
