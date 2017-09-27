d39a724e163cc89fbe889e365ecbdad3f0f34451
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2017-09-27 15:39:49 +0200

    improvend diff for invariants

--- pypipegraph/utils/invariant_diff.py
+++ pypipegraph/utils/invariant_diff.py
@@ -9,7 +9,7 @@ import os
 
 
 def print_usage():
-    print("invariant_diff.py name_of_job_to_diff [--status=prefix]")
+    print("invariant_diff.py name_of_job_to_diff|-all [--status=prefix]")
     print("Dumps the current and the last invariant for this job and then attempts a diff between them")
     print("--status=prefix is used if the .pypipegraph_status_robust file is named differently")
     sys.exit(1)
@@ -30,24 +30,14 @@ job_id_to_compare = None
 for x in sys.argv[1:]:
     if x.startswith('--status='):
         status_prefix = x[x.find('=') + 1:]
+    elif x.startswith('--all'):
+        modus = 'all'
     else:
+        modus = 'job'
         if job_id_to_compare:
             print_usage()
         else:
             job_id_to_compare = x
-
-
-def load_invariant(filename, job_id):
-    with open(filename, 'rb') as op:
-        try:
-            while True:
-                name = pickle.load(op)
-                value = pickle.load(op)
-                if name == job_id:
-                    return value
-        except EOFError as e:
-            pass
-    raise KeyError("Job not found in %s" % filename)
 
 if not os.path.exists(status_prefix):
     if status_prefix == default_status_prefix:
@@ -60,25 +50,67 @@ if not os.path.exists(status_prefix):
             print_status_not_found()
     else:
         print_status_not_found()
+
+if modus == 'job':
+    def load_invariant(filename, job_id):
+        with open(filename, 'rb') as op:
+            try:
+                while True:
+                    name = pickle.load(op)
+                    value = pickle.load(op)
+                    if name == job_id:
+                        return value
+            except EOFError as e:
+                pass
+        raise KeyError("Job not found in %s" % filename)
+
+
     new = load_invariant(status_prefix, job_id_to_compare)
-old = load_invariant(status_prefix + '.old', job_id_to_compare)
+    old = load_invariant(status_prefix + '.old', job_id_to_compare)
 
-print('New:')
-print(new)
-print('')
-if old == new:
-    print('Identicial')
-else:
-    print('Old:')
-    print(old)
+    print('New:')
+    print(new)
     print('')
-    print('Comparison')
+    if old == new:
+        print('Identicial')
+    else:
+        print('Old:')
+        print(old)
+        print('')
+        print('Comparison')
 
-    newf = tempfile.NamedTemporaryFile(prefix='new_')
-    oldf = tempfile.NamedTemporaryFile(prefix='old_')
-    newf.write(str(new))
-    oldf.write(str(old))
-    newf.flush()
-    oldf.flush()
-    p = subprocess.Popen(['diff', newf.name, oldf.name, '-c5'])
-    p.communicate()
+        newf = tempfile.NamedTemporaryFile(prefix='new_')
+        oldf = tempfile.NamedTemporaryFile(prefix='old_')
+        newf.write(str(new).encode('utf-8'))
+        oldf.write(str(old).encode('utf-8'))
+        newf.flush()
+        oldf.flush()
+        p = subprocess.Popen(['diff', newf.name, oldf.name, '-c5'])
+        p.communicate()
+elif modus == 'all':
+    with open(status_prefix, 'rb') as op:
+        new = {}
+        try:
+            while True:
+                key = pickle.load(op)
+                value = pickle.load(op)
+                new[key] = value
+        except EOFError:
+            pass
+    with open(status_prefix + '.old', 'rb') as op:
+        old = {}
+        try:
+            while True:
+                key = pickle.load(op)
+                value = pickle.load(op)
+                old[key] = value
+        except EOFError:
+            pass
+    print('Jobs having a status for the first time')
+    for x in sorted(set(new.keys()).difference(old.keys())):
+        print(x)
+    print('')
+    print('jobs with changed invariants')
+    for job_id in sorted(set(new.keys()).intersection(old.keys())):
+        if new[job_id] != old[job_id]:
+            print(job_id)
