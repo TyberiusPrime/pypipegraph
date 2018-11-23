bd52a39eeac3bae233a1ebe3681784c43401ddba
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2018-11-23 11:37:57 +0100

    missen one readme.md referenc

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name="pypipegraph",
-    version="0.172",
+    version="0.173",
     packages=["pypipegraph"],
     license="MIT",
     url="http://https://github.com/IMTMarburg/pypipegraph",
     author="Florian Finkernagel",
     author_email="finkernagel@imt.uni-marburg.de",
     description="A workflow (job) engine/pipeline for bioinformatics and scientific computing.",
-    long_description=open("README.md").read(),
+    long_description=open("README").read(),
     requires=["kitchen"],
     classifiers=[
         "Programming Language :: Python",
