07047777f52c6f0a8dca2f75f665196164d1e955
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2019-01-09 15:22:37 +0100

    added __version__

--- src/pypipegraph/__init__.py
+++ src/pypipegraph/__init__.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:  # pragma: no cover
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
 from .graph import (
     run_pipegraph,
     new_pipegraph,
